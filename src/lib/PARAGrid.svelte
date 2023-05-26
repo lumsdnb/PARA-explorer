b895d517a6df4e598bdc9e72273c1bbb523608b2
lumsdnb <jeremy@lums.io>
2023-05-26 19:37:41 +0200

    Fix css for now

--- src/lib/PARAGrid.svelte
+++ src/lib/PARAGrid.svelte
@@ -4,7 +4,7 @@
 </script>
 
  <section class="wrapper">    
-     <section>
+     <section class="card">
      <h2>Projects</h2>
      <ul>
        {#each folders.projects.files as file}
@@ -12,80 +12,88 @@
        {/each}
      </ul>
      </section>
- </section>
-  
-<section>
-  <h2>Areas</h2>
-  <ul>
-    {#each folders.areas.files as file}
-      <li>{file}</li>
-    {/each}
-  </ul>
+     
+     <section>
+       <h2>Areas</h2>
+       <ul>
+         {#each folders.areas.files as file}
+         <li>{file}</li>
+         {/each}
+        </ul>
+      </section>
+      
+      <section>
+        <h2>Resources</h2>
+        <ul>
+          {#each folders.resources.files as file}
+          <li>{file}</li>
+          {/each}
+        </ul>
 </section>
-  
-<section>
-  <h2>Resources</h2>
-  <ul>
-    {#each folders.resources.files as file}
-      <li>{file}</li>
-    {/each}
-  </ul>
-</section>
-  
+
 <section>
   <h2>Archive</h2>
   <ul>
     {#each folders.archive.files as file}
-      <li>{file}</li>
+    <li>{file}</li>
     {/each}
   </ul>
 </section>
 
+</section>
 
 <style>
+  * {
+    box-sizing: border-box;
+  }
+  
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+    background-color: #2323;
+    color: #EEE;
+  }
+
+  @media screen and (max-width: 600px) {
     .wrapper {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 20px;
+      grid-template-columns: 1fr;
     }
+  }
 
-    section {
-        background: #232323;
-        border-radius: 5px;
-        padding: 20px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
 
-    h2 {
-        margin-top: 0;
-        color: #EEE;
-        font-size: 1.5em;
-        border-bottom: 1px solid #ccc;
-        padding-bottom: 10px;
-    }
+  section {
+    color: #232323;
+    border-radius: 5px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
 
-    ul {
-        list-style: none;
-        padding: 0;
-        margin: 20px 0 0 0;
-    }
 
-    ul {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        gap: 10px;
-        list-style: none;
-        padding: 0;
-        margin: 20px 0 0 0;
-    }
-    
-    li {
-        width: 15vw;
-        background: #EEE;
-        color: #232323;
-        border-radius: 3px;
-        margin-bottom: 10px;
-        padding: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
-    }
+  h2 {
+    margin-top: 0;
+    color: #EEE;
+    font-size: 1.5em;
+    padding-bottom: 10px;
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 10px;
+    list-style: none;
+    padding: 0;
+    margin: 20px 0 0 0;
+  }
+
+  li {
+    background: #232323;
+    color: #EEE;
+    border-radius: 3px;
+    margin-bottom: 10px;
+    padding: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+    overflow: auto;
+    min-width: 0;
+  }
 </style>
