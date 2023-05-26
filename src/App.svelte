7fdeaa814e6373ac2245c538d8181b157a79f6c3
lumsdnb <jeremy@lums.io>
2023-05-27 00:00:40 +0200

    Add inbox feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,34 @@
 <script lang="ts">
-	import { writable } from 'svelte/store';
-	import type { Writable } from 'svelte/store';
-	import type { FolderDataWrapper } from './types/types';
-	import ParaGrid from './lib/PARAGrid.svelte';
+  import { writable } from 'svelte/store';
+  import type { Writable } from 'svelte/store';
+  import type { FolderDataWrapper } from './types/types';
+  import ParaGrid from './lib/PARAGrid.svelte';
   import { onMount } from 'svelte';
-  
-	const folders: Writable<FolderDataWrapper> = writable({
-	  projects: { files: [], lastChanged: '' },
-	  areas: { files: [], lastChanged: '' },
-	  resources: { files: [], lastChanged: '' },
-	  archive: { files: [], lastChanged: '' },
-	});
-  
-	// Fetch function to get data from the server.
-	async function fetchParaData() {
-	  const response = await fetch('http://127.0.0.1:3000/getPARA');
-	  const data = await response.json();
-	  folders.set(data);
-	}
-  
-	// Fetch data when the component is created.
-	onMount(fetchParaData);
-	
-</script>
-  
-<main>
-	<h1>welcome to your second brain!</h1>
+  import InboxAdder from './lib/InboxAdder.svelte';
+  import InboxViewer from './lib/InboxViewer.svelte';
 
-	<ParaGrid folders={$folders}/>
+  const folders: Writable<FolderDataWrapper> = writable({
+    inbox: { files: [] },
+    projects: { files: [] },
+    areas: { files: [] },
+    resources: { files: [], lastChanged: '' },
+    archive: { files: [], lastChanged: '' },
+  });
+
+  // Fetch function to get data from the server.
+  async function fetchParaData() {
+    const response = await fetch('http://127.0.0.1:3000/getPARA');
+    const data = await response.json();
+    folders.set(data);
+  }
+
+  // Fetch data when the component is created.
+  onMount(fetchParaData);
+</script>
+
+<main>
+  <h1>welcome to your second brain!</h1>
+  <InboxAdder />
+  <InboxViewer folder={$folders.inbox} />
+  <ParaGrid folders={$folders} />
 </main>
---
+++ src/lib/InboxViewer.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let folder:any
+    console.log(folder);
+    
+</script>
+inbox here
