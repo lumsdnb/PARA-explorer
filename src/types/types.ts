cc5f3f665ef90f0e13780720541ef2b70a98c457
lumsdnb <jeremy@lums.io>
2023-05-27 11:56:01 +0200

    Add to type, comment for later

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,11 +1,13 @@
 export type FolderDataStructure = {
   files: string[];
-  lastChanged: string;
 };
 
 export type FolderDataWrapper = {
+  inbox?: FolderDataStructure;
   projects: FolderDataStructure;
   areas: FolderDataStructure;
   resources: FolderDataStructure;
   archive: FolderDataStructure;
 };
+
+//a rename might be in order, and i need a type for markdown files
