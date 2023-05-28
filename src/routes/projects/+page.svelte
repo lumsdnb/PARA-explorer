9eb6667c57715658ee7f8e16c9968fcdfea734d1
lums <jeremy@lums.io>
2023-05-29 01:12:37 +0200

    add placeholder projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,2 +1,24 @@
-Your current projects with their associated tasks. This should only contain the active projects to
-reduce clutter.
+<script>
+	import ProjectCardMedium from '../../components/ProjectCardMedium.svelte';
+</script>
+
+<h1>projects</h1>
+Your current projects with their associated tasks. This should only contain the active projects to reduce
+clutter.
+
+<div class="task-list">
+	<ProjectCardMedium />
+	<div class="spacer" />
+	<ProjectCardMedium />
+	<div class="spacer" />
+	<ProjectCardMedium />
+	<div class="spacer" />
+	<ProjectCardMedium />
+	<div class="spacer" />
+</div>
+
+<style>
+	.spacer {
+		height: 5px;
+	}
+</style>
