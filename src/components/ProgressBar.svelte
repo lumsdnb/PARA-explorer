2efa96b204afacc38f67a29428656abb3ebe6cda
lums <jeremy@lums.io>
2023-05-29 01:10:17 +0200

    create progress bar component

---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let value = 0;
+</script>
+
+<div class="progress-bar">
+	<div class="progress-line" style="width: {value}%" />
+</div>
+
+<style>
+	.progress-bar {
+		height: 16px;
+		width: 100%;
+		position: relative;
+		border: 2px solid #000;
+		overflow: hidden;
+	}
+
+	.progress-line {
+		height: 100%;
+		background-color: #42b983;
+		width: 0%;
+		transition: width 0.5s ease-in-out;
+	}
+</style>
